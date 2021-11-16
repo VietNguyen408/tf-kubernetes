af6040d24097abe23406a6af4cb0f28ace06472c
Viet Nguyen <nguyentrungviet04082001@gmail.com>
2021-11-16 12:32:46 +0100

    update

--- helm_release.tf
+++ helm_release.tf
@@ -9,7 +9,27 @@ provider "helm" {
 }
 
 resource "helm_release" "mysql" {
-  name       = "mysql"
+  name       = "my-release"
   repository = "https://charts.bitnami.com/bitnami"
-  chart      = "mysql"
+  chart      = "bitnami/mysql"
+
+//  values = [
+//    file("${path.module}/mysql-values.yaml")
+//  ]
+
+  set {
+    name  = "cluster.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "metrics.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "service.annotations.prometheus\\.io/port"
+    value = "9127"
+    type  = "string"
+  }
 }
