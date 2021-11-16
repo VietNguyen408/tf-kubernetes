28b2b511d410d5e652df3839f3c0a8da88781812
Viet Nguyen <nguyentrungviet04082001@gmail.com>
2021-11-16 15:50:22 +0100

    update

--- kubernetes.tf
+++ kubernetes.tf
@@ -8,10 +8,6 @@ variable "access_token" {
 
 terraform {
   required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "~> 2.0.1"
-    }
     google = {
       source  = "hashicorp/google"
       version = ">=3.52.0"
